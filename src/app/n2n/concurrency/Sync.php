2231549bc58079805ff5166d811cd9f722885e99
david <meier@hnm.ch>
2024-04-22 16:04:05 +0200

    Initial Commit

---
+++ src/app/n2n/concurrency/Sync.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace n2n\concurrency;
+
+class Sync {
+
+}
---
+++ src/test/n2n/concurrency/SyncTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace n2n\concurrency;
+
+use PHPUnit\Framework\TestCase;
+
+class SyncTest extends TestCase {
+
+}
