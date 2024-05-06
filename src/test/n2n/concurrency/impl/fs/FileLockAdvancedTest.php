74324e4fb69b6b083d0ac5a6ea06b0931de5cd7b
Andreas von Burg <a@von-burg.net>
2024-05-06 14:56:07 +0200

    advanced tests for extreme slow cpus

--- src/test/n2n/concurrency/impl/fs/FileLockAdvancedTest.php
+++ src/test/n2n/concurrency/impl/fs/FileLockAdvancedTest.php
@@ -207,7 +207,7 @@ class FileLockAdvancedTest extends TestCase {
 		$blockingLock = Sync::byFileLock($fsPath);
 		$this->assertTrue($blockingLock->acquireNb());
 
-		$lock = Sync::byFileLock($fsPath)->setAcquireAttempts(3)->setSleepUs(15000);
+		$lock = Sync::byFileLock($fsPath)->setAcquireAttempts(3)->setSleepUs(30000);
 
 		$startS = microtime(true);
 
@@ -220,9 +220,9 @@ class FileLockAdvancedTest extends TestCase {
 		$deltaS = (microtime(true)) - $startS;
 
 		// only 2 sleep attempts
-		$this->assertGreaterThanOrEqual(30000, $deltaS * 1000000);
+		$this->assertGreaterThanOrEqual(60000, $deltaS * 1000000);
 		// less than 3 sleep attempts
-		$this->assertLessThan(45000, $deltaS * 1000000);
+		$this->assertLessThan(90000, $deltaS * 1000000);
 	}
 
 	/**
